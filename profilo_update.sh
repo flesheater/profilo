6fd4a96971f769241e49bf6e2e7c57b73c271756
nikolay_compuccino <nikolay@compuccino.com>
2014-05-14 14:55:10 +0200

    Some fixes to the update bash script

--- profilo_update.sh
+++ profilo_update.sh
@@ -3,6 +3,6 @@ green='\e[0;32m'
 endColor='\e[0m'
 
 clear
-rm -r modules/{views,admin_menu,ctools,devel,entity,i18n,module_filter,pathauto,rules,token,transliteration,variable}
-drush dl views admin_menu ctools devel entity i18n module_filter pathauto rules token transliteration variable --destination=modules
+rm -rf modules/{views,admin_menu,ctools,devel,entity,i18n,module_filter,pathauto,rules,token,transliteration,variable}
+drush dl views admin_menu ctools devel entity i18n module_filter pathauto rules token transliteration variable --destination=profiles/profilo/modules
 echo -e "${green}Update of the profilo profile completed :) You may open a beer now :) \n${endColor}"
